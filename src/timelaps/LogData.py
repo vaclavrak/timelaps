0b9c60f0e25056a5b02e523fb554da05c03362d9
Rak Václav <rak@avast.com>
2018-04-16 22:53:20 +0200

    fixed WD

--- src/timelaps/LogData.py
+++ src/timelaps/LogData.py
@@ -8,14 +8,18 @@ class BasicLogDataException(Exception):
 
 class BasicLogData(object):
     _k = {}
+    _v = {}
     _config = None
 
     def __init__(self):
         self._k = {}
 
     def add(self, m, v):
-        self._k[m] = v
-
+        kk = "_".join(m)
+        self._k[kk] = m
+        self._v[kk] = v
+        return self
+    
     def set_config(self, cfg: Configurator):
         if not isinstance(cfg, Configurator):
             raise BasicLogDataException("Invalid config type")
@@ -33,6 +37,6 @@ class BasicLogData(object):
             src_module = sys.modules["timelaps.Senders.{cls}".format(cls=sndr)]
             src_class = getattr(src_module, "{cls}Sender".format(cls=sndr))
             sender_cls = src_class().set_config(self.config).setup()
-            for k in self._k:
-                sender_cls.send_data(k, self._k[k], k)
+            for k in self._k.keys():
+                sender_cls.send_data(self._k[k], self._v[k], k)
             sender_cls.flush({})
